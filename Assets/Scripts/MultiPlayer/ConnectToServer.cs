744d2d8d6b44e3979d84b7bba1067b2847954870
Szyrrogi <szyrrogi@hotmail.com>
2024-10-21 22:47:50 +0200

    lekkie poprawki

--- Assets/Scripts/MultiPlayer/ConnectToServer.cs
+++ Assets/Scripts/MultiPlayer/ConnectToServer.cs
@@ -25,4 +25,6 @@ public class ConnectToServer : MonoBehaviourPunCallbacks
         SceneManager.LoadScene("Lobby");
     }
 
+   
+
 }
--- Assets/Scripts/Player/SpawnPlayer.cs
+++ Assets/Scripts/Player/SpawnPlayer.cs
@@ -2,19 +2,71 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-//
-public class SpawnPlayer : MonoBehaviour
+using Photon.Realtime;
+
+public class SpawnPlayer : MonoBehaviourPunCallbacks
 {
-    public GameObject[] player;
-    public Vector2 position;
+    public GameObject[] player; 
+    public Vector2 position; 
+
+    private int playersReady = 0;
 
     void Start()
     {
-        if(PlayerClass.klasa == 0)
+        SetPlayerReady(true);
+
+        CheckAllPlayersReady();
+    }
+
+    public void SetPlayerReady(bool isReady)    //TO będzie trzeba zmienić bo obecna werjsa jest mało autorska
+    {
+        ExitGames.Client.Photon.Hashtable playerProperties = new ExitGames.Client.Photon.Hashtable();
+        playerProperties["ready"] = isReady;
+        PhotonNetwork.LocalPlayer.SetCustomProperties(playerProperties);
+    }
+
+    private void CheckAllPlayersReady()
+    {
+        int playerCount = PhotonNetwork.PlayerList.Length;
+
+        playersReady = 0;
+        foreach (Player player in PhotonNetwork.PlayerList)
         {
-            PlayerClass.klasa = Random.Range(1,5);
+            if (player.CustomProperties.TryGetValue("ready", out object isReady) && (bool)isReady)
+            {
+                playersReady++;
+            }
         }
-        GameObject playerInstance = PhotonNetwork.Instantiate(player[PlayerClass.klasa-1].name, position, Quaternion.identity);
+
+        if (playersReady == playerCount)
+        {
+            AllPlayersReady();
+        }
+    }
+
+    private void AllPlayersReady()
+    {
+        Debug.Log("Wszyscy gracze są gotowi! Tworzenie postaci...");
+
+        if (PlayerClass.klasa == 0)
+        {
+            PlayerClass.klasa = Random.Range(1, 5);
+        }
+
+        GameObject playerInstance = PhotonNetwork.Instantiate(player[PlayerClass.klasa - 1].name, position, Quaternion.identity);
         playerInstance.GetComponent<PhotonView>().TransferOwnership(PhotonNetwork.LocalPlayer);
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, ExitGames.Client.Photon.Hashtable changedProps)
+    {
+        if (changedProps.ContainsKey("ready"))
+        {
+            CheckAllPlayersReady();
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        CheckAllPlayersReady();
     }
 }
